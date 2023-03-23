668d2dc3329884c095364ebd3cc05ccc6060d923
Abdul Sagheer <73389096+abdulsagheer@users.noreply.github.com>
2023-03-23 15:16:03 +0530

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./styles/index.scss";
+import { store } from "./redux/store";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
 		<BrowserRouter>
-			<App />
+			<Provider store={store}>
+				<App />
+			</Provider>	
 		</BrowserRouter>
 	</React.StrictMode>
 );
